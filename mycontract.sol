3600ecdc3fbf90463d9b3741e8ebe00880d27f1f
Allen Liou <allenl@outlook.com>
2022-02-17 17:37:07 -0800

    mapping

--- mycontract.sol
+++ mycontract.sol
@@ -3,22 +3,25 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract Splitwise {
 
-    struct Transaction {
-        address debtor;
-	    address creditor;
-        uint32 amount;
-    }
+    // struct AmountOwed {
+    //     address debtor;
+	//     address creditor;
+    //     uint32 amount;
+    // }
 
-    event NewTransaction(address _debtor, address _creditor, uint _amount);
+    event NewTransaction(string key, address _debtor, address _creditor, uint _amount);
 
-    Transaction[] public transactions; 
+    // Transaction[] public transactions; 
+    mapping (string => uint32) currentState;
 
     function _addIOU(address _creditor, uint32 _amount) public {
-        transactions.push(Transaction(msg.sender, _creditor, _amount));
-        emit NewTransaction(msg.sender, _creditor, _amount);
+        string memory key = string(abi.encodePacked(msg.sender, _creditor));
+        currentState[key] = _amount;
+        emit NewTransaction(key, msg.sender, _creditor, currentState[key]);
     }
 
-    function _lookup (address _creditor, address _debtor) public view {
-
+    function _lookup (address _creditor, address _debtor) public view returns (uint) {
+        string memory key = string(abi.encodePacked(_debtor, _creditor));
+        return uint(currentState[key]);
     }
 }
